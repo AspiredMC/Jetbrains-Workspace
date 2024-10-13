a5e673951d7734b7989f0103c3245d281d664b36
IcyEndymion004 <63156236+IcyEndymion004@users.noreply.github.com>
2024-10-13 12:56:24 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,10 +5,7 @@ KEY_DIR="/home/container/sshd"
 
 # Check for SSH host keys and generate them if not found
 if [ ! -f "${KEY_DIR}/ssh_host_rsa_key" ]; then
-    echo "Generating SSH host keys..."
-    ssh-keygen -A -f "${KEY_DIR}/ssh_host_" # Generate keys in the specified directory
-    chmod 600 ${KEY_DIR}/ssh_host_*_key
-    chmod 644 ${KEY_DIR}/ssh_host_*_key.pub
+    echo "SSH Keys not found PLEASE REINSTALL"
 fi
 
 # Start the SSH daemon
