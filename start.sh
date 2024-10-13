c51d7bcb12ac4973812d9116b39ed64bb933786d
IcyEndymion004 <63156236+IcyEndymion004@users.noreply.github.com>
2024-10-13 11:51:39 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-service docker start
-cd /var/www/$PROJECT_NAME
-docker-compose up -d
+if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
+    echo "Generating SSH host keys..."
+    ssh-keygen -A
+fi
+
+echo "Starting SSH daemon on port ${SSH_PORT}..."
+exec /usr/sbin/sshd -D -p "${SSH_PORT}"
